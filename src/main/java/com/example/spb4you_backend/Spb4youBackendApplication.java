b4a6570c63014bd4107ea26e7be5a2bda33f3c53
svetabagaeva5 <sgbagaeva.work@gmail.com>
2025-09-10 23:28:28 +0300

    Inital commit

---
+++ src/main/java/com/example/spb4you_backend/Spb4youBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.spb4you_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spb4youBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spb4youBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spb4you_backend/Spb4youBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spb4you_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spb4youBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
